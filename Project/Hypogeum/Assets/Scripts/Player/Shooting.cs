50eaa65a2310cc922bdc0b741fb3ba88858986ae
Michele Maione <mikymaione@hotmail.it>
2019-05-07 11:21:50 +0200

    sparo network

--- Project/Hypogeum/Assets/Scripts/Player/Shooting.cs
+++ Project/Hypogeum/Assets/Scripts/Player/Shooting.cs
@@ -6,23 +6,46 @@ Contributors:
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
 */
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Shooting : MonoBehaviour
+public class Shooting : NetworkBehaviour
 {
 
     public float speed = 20;
     public GameObject projectilePrefab;
 
+    private bool canShoot = true;
+
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            var projectile = Instantiate(projectilePrefab, transform.position, transform.rotation) as GameObject;
-            var projectile_RB = projectile.GetComponent<Rigidbody>();
-            projectile_RB.velocity = transform.TransformDirection(Vector3.forward * speed);            
-        }       
+        if (isLocalPlayer)
+            if (canShoot && Input.GetMouseButtonDown(0))
+            {
+                canShoot = false;
+                CmdShoot();
+                StartCoroutine(RechargeWeapon());
+            }
+    }
+
+    [Command]
+    public void CmdShoot()
+    {
+        var projectile = Instantiate(projectilePrefab, transform.position, transform.rotation) as GameObject;        
+
+        NetworkServer.SpawnWithClientAuthority(projectile, this.gameObject);
+
+        var projectile_RB = projectile.GetComponent<Rigidbody>();
+        projectile_RB.velocity = transform.TransformDirection(Vector3.forward * speed);
+    }
+
+    private IEnumerator RechargeWeapon()
+    {
+        yield return new WaitForSeconds(0.5f);
+        canShoot = true;
+        StopCoroutine(RechargeWeapon());
     }
 
 }
