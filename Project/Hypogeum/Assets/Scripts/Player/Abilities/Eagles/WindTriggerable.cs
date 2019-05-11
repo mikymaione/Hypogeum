6f574cde5d4dfd99fc50b8e74aed55c13a15bebe
AndreaC <andrea.carrarini.18@gmail.com>
2019-05-11 14:43:48 +0200

    ciao

---
+++ Project/Hypogeum/Assets/Scripts/Player/Abilities/Eagles/WindTriggerable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* creates a wind shield around the car deflecting incoming projectiles
+ * it is the "WindAbility" GameObject in EaglesCar, it has a boxCollider to enable when using the ability
+ * since the WindAbility BoxCollider is not much greater than the car normal collider, 
+ * it should be good to use it as the collision collider for collisions 
+ * */
+public class WindTriggerable : MonoBehaviour
+{
+
+    public BoxCollider windBoxCollider;
+
+    public void Initialize()
+    {
+        windBoxCollider = GameObject.FindGameObjectWithTag("WindShield").GetComponent<BoxCollider>();
+    }
+
+    public void ActivateWind()
+    {
+        windBoxCollider.enabled = true;
+    }
+
+    public void Update()
+    {
+		
+    }
+}
--- Project/Hypogeum/Assets/Scripts/Player/Abilities/Shark/BiteAbility.cs
+++ Project/Hypogeum/Assets/Scripts/Player/Abilities/Shark/BiteAbility.cs
@@ -33,5 +33,4 @@ public class BiteAbility : Ability
     {
         biteTriggerable.Bite();
     }
-
 }
