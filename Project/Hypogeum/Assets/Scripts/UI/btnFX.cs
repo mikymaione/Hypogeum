563664ad41db3497757077927aba3ab85bec5667
AndreaC <andrea.carrarini.18@gmail.com>
2019-06-05 17:23:18 +0200

    UI ready

---
+++ Project/Hypogeum/Assets/Scripts/UI/btnFX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class btnFX : MonoBehaviour
+{
+	public Color cursorInsideColor = new Color(200.0f / 255.0f, 200.0f / 255.0f, 200.0f / 255.0f, 128.0f/ 255.0f);
+	public Color cursorOutsideColor = new Color(255.0f / 255.0f, 255.0f / 0.0f, 255.0f / 255.0f, 255.0f/ 255.0f);
+	//public Button button;
+	public Text text;
+
+    public void OnCursorEnter()
+	{
+		//button = GetComponent<Button>();
+		text = GetComponentInChildren<Text>();
+		text.color = cursorInsideColor;
+	}
+
+	public void onCursorExit()
+	{
+		//button = GetComponent<Button>();
+		text = GetComponentInChildren<Text>();
+		text.color = cursorOutsideColor;
+	}
+}
