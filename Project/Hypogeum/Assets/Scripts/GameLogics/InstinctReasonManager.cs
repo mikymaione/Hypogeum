cb2790f638e9ffa6610c43d9a67a8c9283d25e42
Michele Maione <mikymaione@hotmail.it>
2019-06-24 11:52:48 +0200

    istinct manger tolto islocalplayer

--- Project/Hypogeum/Assets/Scripts/GameLogics/InstinctReasonManager.cs
+++ Project/Hypogeum/Assets/Scripts/GameLogics/InstinctReasonManager.cs
@@ -18,12 +18,15 @@ public class InstinctReasonManager : NetworkBehaviour
 
     void Start()
     {
-        instinct.SetActive(false);
-        reason.SetActive(false);
-        damage.SetActive(false);
-        control.SetActive(false);
+        if (isLocalPlayer)
+        {
+            instinct.SetActive(false);
+            reason.SetActive(false);
+            damage.SetActive(false);
+            control.SetActive(false);
 
-        audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource = gameObject.GetComponent<AudioSource>();
+        }
     }
 
     [Command] //solo host
