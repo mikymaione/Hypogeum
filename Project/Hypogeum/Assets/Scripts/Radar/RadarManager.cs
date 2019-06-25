60ccd138e6542d7cfbe02e81cf0775be2dd99d6c
AndreaC <andrea.carrarini.18@gmail.com>
2019-06-25 21:38:36 +0200

    minimap works

--- Project/Hypogeum/Assets/Scripts/Radar/RadarManager.cs
+++ Project/Hypogeum/Assets/Scripts/Radar/RadarManager.cs
@@ -6,30 +6,9 @@ using UnityEngine.UI;
 
 public class RadarManager : NetworkBehaviour
 {
-	private GameObject[] cars;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-		cars = GameObject.FindGameObjectsWithTag("car");
-		for (int i = 0; i < cars.Length; i++)
-		{
-			if (GB.Animal.Value == cars[i].GetComponent<GeneralCar>().AnimalType)
-			{
-				cars[i].transform.Find("Sphere").GetComponent<MeshRenderer>().enabled = false;
-			}
-			else
-			{
-				cars[i].transform.Find("cono").GetComponent<MeshRenderer>().enabled = false;
-			}
-		}
-
+	// Start is called before the first frame update
+	void Start()
+	{
 		var rawImage = gameObject.GetComponentInChildren<RawImage>().texture = GB.getAnimalRenderTexture(GB.Animal.Value, GB.GameType.Value);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
 }
